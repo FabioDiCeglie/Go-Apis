5f6c0e7e2fa1c89edce560eccb182db2560fbd84
FabioDiCeglie <fabiod@backbase.com>
2023-05-29 12:27:49 +0200

    feat: init database in main

--- go-fiber-crm/main.go
+++ go-fiber-crm/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/fabio/go-fiber/database"
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
 )
 
 func setUpRoutes(app *fiber.App) {
 	app.Get(GetLeads)
+	app.Get(GetLead)
 	app.Post(NewLead)
 	app.Delete(DeleteLead)
 }
 
+func initDatabase() {
+	var db *gorm.DB
+	database.Connect()
+	db = database.GetDB()
+	db.AutoMigrate()
+	fmt.Println("Database Migrated")
+}
+
 func main() {
 	app := fiber.New()
+	initDatabase()
 	setUpRoutes(app)
+	app.Listen(3000)
+	fmt.Print("Starting server to http://localhost:3000")
+	defer database.DBConn.Close()
 }
