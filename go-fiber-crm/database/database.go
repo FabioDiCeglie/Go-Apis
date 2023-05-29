2898c8f4b940544cada392401ece81c0c101f783
FabioDiCeglie <fabiod@backbase.com>
2023-05-29 12:27:37 +0200

    feat: setting up connection with db

---
+++ go-fiber-crm/database/database.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	dsn := "host=mouse.db.elephantsql.com user=ydgmnplr password=jMKkqF5eKoRj5MrTnJRAw6zpXGGQPMcn dbname=ydgmnplr port=5432"
+	d, err := gorm.Open("postgres", dsn)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connection opened to database")
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
