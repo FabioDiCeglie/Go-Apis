e4b45c32e4d8dedbb87fd9bd722683829473feb9
FabioDiCeglie <fabiod@backbase.com>
2023-06-01 17:51:30 +0200

    feat: query links from db

--- go-graphql/graph/schema.resolvers.go
+++ go-graphql/graph/schema.resolvers.go
@@ -56,13 +56,9 @@ func (r *mutationResolver) RefreshToken(ctx context.Context, input model.Refresh
 
 // Links is the resolver for the links field.
 func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
+	db := database.Db
 	var links []*model.Link
-	dummyLink := model.Link{
-		Title:   "our dummy link",
-		Address: "https://address.org",
-		User:    &model.User{Name: "admin"},
-	}
-	links = append(links, &dummyLink)
+	db.Find(&links)
 	return links, nil
 }
 
