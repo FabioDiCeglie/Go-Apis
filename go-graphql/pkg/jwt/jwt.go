7beca668d5eba60cff860669a0a94ebefde2a4fc
FabioDiCeglie <fabiod@backbase.com>
2023-06-02 09:57:29 +0200

    feat: create functionality for hash password

--- go-graphql/pkg/jwt/jwt.go
+++ go-graphql/pkg/jwt/jwt.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -13,7 +14,7 @@ var (
 
 // GenerateToken generates a jwt token and assign a username to it's claims and return it
 func GenerateToken(username string) (string, error) {
-	token := jwt.New(jwt.SigningMethodSH256)
+	token := jwt.New(jwt.SigningMethodHS256)
 
 	claims := token.Claims.(jwt.MapClaims)
 
@@ -38,4 +39,16 @@ func ParseToken(tokenStr string) (string, error) {
 	} else {
 		return "", err
 	}
+}
+
+// HashPassword hashes given password
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+// CheckPassword hash compares raw password with it's hashed values
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
